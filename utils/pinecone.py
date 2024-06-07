86ecbe5630e91cee66c01444965136c7a9b7e3c8
dev-abuke <abubekershamil123@gmail.com>
2024-06-08 00:09:01 +0300

    pinecone helper class

---
+++ utils/pinecone.py
@@ -0,0 +1,39 @@
+import time
+import os
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+
+class PineconeVector:
+    def __init__(self, index_name):
+        self.index_name = index_name
+        self.embeddings = OpenAIEmbeddings()
+    def create_pinecone_index(self):
+        pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+        pc = Pinecone(api_key=pinecone_api_key)
+
+        existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+
+        if self.index_name not in existing_indexes:
+            pc.create_index(
+                name=self.index_name,
+                dimension=1536,
+                metric="cosine",
+                spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+            )
+            while not pc.describe_index(self.index_name).status["ready"]:
+                time.sleep(1)
+
+        index = pc.Index(self.index_name)
+
+        return index, pc
+    
+    def insert(self, split_documents):
+        pinecone_vectorstore = PineconeVectorStore.from_documents(
+            split_documents, 
+            self.embeddings, 
+            index_name=self.index_name
+        )
+
+        return pinecone_vectorstore
