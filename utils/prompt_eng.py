6318cef1143d4e0ebba064d4cbd8bd978703fda9
dev-abuke <abubekershamil123@gmail.com>
2024-06-08 01:05:50 +0300

    detach augumentation from vector search

--- utils/prompt_eng.py
+++ utils/prompt_eng.py
@@ -1,11 +1,13 @@
 from langchain_pinecone import PineconeVectorStore
 
 
-def augment_prompt(query: str, pinecone_vector_db: PineconeVectorStore):
-    # get top 3 results from knowledge base
-    results = pinecone_vector_db.similarity_search(query, k=3)
-    # get the text from the results
-    source_knowledge = "\n".join([x.page_content for x in results])
+def augment_prompt(query: str, source_knowledge: str) -> str:
+    # check if the query is None or empty
+    if not query:
+        raise ValueError("Query cannot be empty")
+    # check if the pinecone_vector_db object is None
+    if not source_knowledge:
+        raise ValueError("Source knowledge object cannot be None")
     # feed into an augmented prompt
     augmented_prompt = f"""Using the contexts below, answer the query.
     Contexts:
