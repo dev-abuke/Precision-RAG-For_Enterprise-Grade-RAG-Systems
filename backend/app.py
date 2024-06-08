ac90af083776b7b86f70ad9e8c65346594076a1f
dev-abuke <abubekershamil123@gmail.com>
2024-06-08 15:46:19 +0300

    add multiple prompt generation

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,29 @@
 from flask import Flask, render_template, request
+import os, sys
+from openai import OpenAI
+from utility.env_manager import get_env_manager
+from utility.pinecone import PineconeVector
+from utility.pdf_ingestion import split_documents
+from evaluation._data_generation import get_completion
+from evaluation._data_generation import file_reader
+from evaluation._evaluation import evaluate
+from prompts.prompts import get_test_prompts
+from utility.rag import RAG
+
+sys.path.append("./uploaded_docs/")
+
+PINECONE_INDEX_NAME = "pdf-doc"
+pcv = PineconeVector(PINECONE_INDEX_NAME)
+rag = RAG()
+
+env_manager = get_env_manager()
+zx = OpenAI(api_key=env_manager['openai_keys']['OPENAI_API_KEY'])
+
+split_document = split_documents(doc_path="./uploaded_docs/10 Academy Cohort B - Weekly Challenge_ Week - 7.pdf")
+
+index, pc = pcv.create_pinecone_index() 
+
+vector = pcv.insert(split_document)
 
 app = Flask(__name__)
 
@@ -8,11 +33,38 @@ def home():
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    description = request.form['description']
-    scenarios = request.form['scenarios']
-    # Here you will integrate the prompt generation algorithm
-    generated_prompts = ["Prompt 1", "Prompt 2"]  # Placeholder for generated prompts
-    return render_template('result.html', prompts=generated_prompts)
+    data = request.get_json()
+    question = data.get('question')
+
+    context_message = pcv.search_vectorstore(str(question))
+
+    test_prompt_generator = get_test_prompts(num=5, Context=context_message, Objective=question)
+
+    array_of_test_prompts = rag.generate_answer(test_prompt_generator)
+
+    # context_message = file_reader("prompts/context.txt") # TODO add context with a given pdf
+    prompt_message = file_reader("prompts/generic-evaluation-prompt.txt")
+
+    context = str(context_message)
+    prompt = str(prompt_message)
+    
+    prompt_score = []
+    
+    for test_prompt in array_of_test_prompts.splitlines():
+        print("The Prompt is :: ", test_prompt)
+
+        user_message = str(test_prompt)
+    
+        classification, accuracy = evaluate(prompt, user_message, context)
+
+        print(classification, accuracy)
+
+        # add to a dictionary
+        prompt_score.append({'prompt': test_prompt, 'classification': classification, 'accuracy': accuracy})
+
+
+    # return 200 response and the classification and accuracy
+    return {'message': prompt_score, "status": True}
 
 if __name__ == '__main__':
     app.run(debug=True)
