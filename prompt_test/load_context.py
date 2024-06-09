d6f0d924bf30d99b51bed024e4cb916e619fa0fd
dev-abuke <abubekershamil123@gmail.com>
2024-06-10 00:29:05 +0300

    feat: context retrieval for testing rag

---
+++ prompt_test/load_context.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+# Add the directory containing 'utils' to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.pinecone import PineconeVector
+
+def get_var(var_name, prompt, other_vars):
+    query = other_vars['query']
+    # Fetch relevant documents and join them into a string result.
+    PINECONE_INDEX_NAME = os.environ.get("PINECONE_INDEX_NAME")
+
+    pcv = PineconeVector(PINECONE_INDEX_NAME)
+
+    query_result = pcv.query(query)
+
+    # for doc in query_result:
+    #     print(f"The score is {doc['score']} : {doc['text']}")
+
+    text = query_result[0]["text"]
+
+    # print(f"The text is {text}")
+
+    result = {
+        "output": text,
+    }
+    # Include error handling and token usage reporting as needed
+    # if some_error_condition:
+    #     result['error'] = "An error occurred during processing"
+    #
+    # if token_usage_calculated:
+    #     result['tokenUsage'] = {"total": token_count, "prompt": prompt_token_count, "completion": completion_token_count}
+
+    return result
+
+# if __name__ == "__main__":
+#     call_api("Who are the tutors for this weeks challenge?", {}, {})
